faa66b5d4e13d4c5dd6844cc9b638141b5fc9ed0
Cabeludo <brepimentel2001@gmail.com>
2023-07-20 19:40:19 -0300

    processo feito, falta tratamento de repetidas

--- t.py
+++ t.py
@@ -5,6 +5,7 @@ import keyboard as k
 from os.path import exists
 import os
 from json import load
+from selenium.webdriver.common.keys import Keys
 
 path = "NotasTestes.txt"
 linhas:list[str]
@@ -27,28 +28,18 @@ if exists("chromedriver.exe"):
 elif exists("chromedriver"):
     chrome_driver = "./chromedriver"
 
-if chrome_driver == "./chromedriver":
-    command = f'bash {chrome_path}google-chrome --remote-debugging-port=8989 --user-data-dir="{os.path.dirname(__file__)}/ChromeProfile"'
-elif chrome_driver == "chromedriver.exe":
-    command = f'cmd /k \"{chrome_path}/google-chrome.exe --remote-debugging-port=8989 --user-data-dir=\"{os.path.dirname(__file__)}/ChromeProfile\""'
-os.system(command)
-
-while True:
-    print("Pressione Ctrl e Shift quando estiver na aba de notas no navegador que abriu.")
-    if k.is_pressed("Ctrl+Shift"):
-        break
-
 chrome_options = Options()
 chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:8989")
 #Change chrome driver path accordingly
 driver = webdriver.Chrome(options=chrome_options)
 
-for i in linhas:
-    value = i.replace("httpsÇ;;www.nfce.fazenda.sp.gov.br;qrcode:p=", '')
+for n, i in enumerate(linhas):
+    value = linhas[n].replace("httpsÇ;;www.nfce.fazenda.sp.gov.br;qrcode:p=", '').replace("httpsÃ‡;;www.nfce.fazenda.sp.gov.br;qrcode:p=", "")
     value = value[:44]
     if value.isnumeric():
-        driver.find_element(select.By.XPATH, "//*[@id=\"ac03504b9f4b446da5fa66dcadb8aba8\"]").send_keys(value)
-        k.press("Enter")
+        el = driver.find_element(select.By.XPATH, "/html/body/form/div[4]/div[6]/div[2]/div[2]/fieldset/div[4]/fieldset/input")
+        el.send_keys(value)
+        el.send_keys(Keys.ENTER)
     else:
         with open("QRErros", "a") as f:
             f.writelines(i)
